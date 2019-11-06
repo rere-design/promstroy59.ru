51de8338801cc6f4969668cab5df69c0b871319d
Aleksandr2296 <saha59ru@mail.ru>
2019-11-06 15:58:08 +0500

    #31992 разбил на 3 страницы

--- rent/index.php
+++ rent/index.php
@@ -213,262 +213,6 @@ global $inc, $current, $refinfo, $cms_object, $root;
     </div>
 </div>
 
-<div class="sev sec_quiz section" id="quiz">
-    <div class="art container">
-        <div class="head2 title">Подберите компрессор для своего бизнеса у нас,<br> со
-            <span>скидкой в 20%!</span></div>
-
-        <div id="app">
-            <div class="info" v-show="completed === false">
-                <div class="percentage">
-                    Пройдено: {{ percent }}
-                </div>
-                <div class="progressbar">
-                    <div class="progressbar_body"
-                         v-bind:style="{width: percentStyle}"></div>
-                </div>
-                <div class="question_number">
-                    Вопрос {{ questionIndex+1 }} из {{ quiz.questions.length+1 }}
-                </div>
-            </div>
-            <div v-for="(question, index) in quiz.questions">
-                <!-- Hide all questions, show only the one with index === to current question index -->
-                <div v-show="index === questionIndex">
-                    <h2>{{ question.text }}</h2>
-                    <ol>
-                        <li v-for="response in question.responses">
-                            <label class="option">
-                                <!-- The radio button has three new directives -->
-                                <!-- v-bind:value sets "value" to "true" if the response is correct -->
-                                <!-- v-bind:name sets "name" to question index to group answers by question -->
-                                <!-- v-model creates binding with userResponses -->
-                                <input type="radio"
-                                       v-bind:name="index"
-                                       v-model="userResponses[index]" v-on:click="next"
-                                > {{response.text}}
-                            </label>
-                        </li>
-                    </ol>
-                </div>
-            </div>
-            <div class="result_controls"
-                 v-show="questionIndex === quiz.questions.length && completed === false">
-                <button v-on:click="submit">
-                    Подтвердить
-                </button>
-                <button v-on:click="restart">
-                    Ответить заново
-                </button>
-            </div>
-
-            <div class="form-quiz" v-show="completed === true">
-                <div class="head3">Спасибо за ответы, куда выслать информацию?</div>
-                <form action="<?= $inc->send; ?>" method="post">
-                    <input type="hidden" name="goalname" value="Информация">
-                    <input type="hidden" name="goal" value="goal_info">
-                    <input type="hidden" name="good" value="">
-                    <div class="items">
-                        <input type="hidden" name="answers" value="">
-                        <div class="item"><input type="email" name="email"
-                                                 placeholder="Электронная почта"></div>
-                        <div class="item"><input type="text" name="phone"
-                                                 placeholder="+7 (____) - ___ - ___ - _">
-                        </div>
-                        <div class="item">
-                            <button type="submit"><span>Отправить письмо</span></button>
-                        </div>
-                    </div>
-                    <label><input type="checkbox" name="politic" value="1"
-                                  data-focus="icon" checked><span
-                            class="icon"></span><span
-                            class="label">Я даю согласие на обработку персональных данных</span></label>
-                </form>
-            </div>
-
-        </div>
-    </div>
-
-</div>
-
-
-<div class="sec_brand section">
-    <div class="container">
-        <div class="sec_brand__wrapper-box">
-            <div class="columns">
-                <div class="column is-4">
-                    <div class="sec_brand__wrapper">
-                        <div class="sec_brand__box">
-                            <img src="data/upload/equipments/list/logo-1.png" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="column is-4">
-                    <div class="sec_brand__wrapper">
-                        <div class="sec_brand__box">
-                            <img src="data/upload/equipments/list/logo-2.png" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="column is-4">
-                    <div class="sec_brand__wrapper">
-                        <div class="sec_brand__box">
-                            <img src="data/upload/equipments/list/logo.svg" width="212" height="50" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-</div>
-
-<script>
-  var quiz = {
-    questions: [
-      {
-        text: "Какой привод компрессора?",
-        responses: [
-          {text: 'Электрический'},
-          {text: 'Дизельный'},
-          {text: 'Не знаю'},
-        ]
-      }, {
-        text: "Какой тип компрессора требуется?",
-        responses: [
-          {text: 'Передвижной'},
-          {text: 'Станционарный'},
-          {text: 'Не знаю'},
-        ]
-      }, {
-        text: "Необходимое рабочее давление?",
-        responses: [
-          {text: 'От 1-7 Бар'},
-          {text: '7-14 Бар'},
-          {text: '14-35 Бар'},
-          {text: 'Не знаю'},
-        ]
-      }, {
-        text: "Требуемая производительность компрессора?",
-        responses: [
-          {text: 'До 5,5 м3 в минуту'},
-          {text: 'от 5,5 – 13 м3 в минуту'},
-          {text: '13- 65 м3 в минуту'},
-          {text: 'Не знаю'},
-        ]
-      }
-    ]
-  };
-
-  new Vue({
-    el: '#app',
-    data: {
-      quiz: quiz,
-      questionIndex: 0,
-      userResponses: Array(quiz.questions.length).fill(false),
-      answers: [],
-      completed: false
-    },
-    computed: {
-      percent: function () {
-        return ((this.questionIndex / this.quiz.questions.length) * 100) + '%'
-      },
-      percentStyle: function () {
-        return ((this.questionIndex / this.quiz.questions.length) * 100 + 5) + '%'
-      }
-    },
-    methods: {
-      next: function (e) {
-        var question = quiz.questions[this.questionIndex].text;
-        var answer = e.target.labels[0].innerText;
-        this.answers.push([question, answer]);
-        this.questionIndex++;
-      },
-      submit: function () {
-        this.completed = true;
-        $('[name=answers]').val(JSON.stringify(this.answers));
-      },
-      restart: function () {
-        this.answers = [];
-        this.questionIndex = 0;
-      }
-    }
-  });
-</script>
-
-<script>
-  var quizs = {
-    questions: [
-      {
-        text: "Какой привод компрессора?",
-        responses: [
-          {text: 'Электрический'},
-          {text: 'Дизельный'},
-          {text: 'Не знаю'},
-        ]
-      }, {
-        text: "Какой тип компрессора требуется?",
-        responses: [
-          {text: 'Передвижной'},
-          {text: 'Станционарный'},
-          {text: 'Не знаю'},
-        ]
-      }, {
-        text: "Необходимое рабочее давление?",
-        responses: [
-          {text: 'От 1-7 Бар'},
-          {text: '7-14 Бар'},
-          {text: '14-35 Бар'},
-          {text: 'Не знаю'},
-        ]
-      }, {
-        text: "Требуемая производительность компрессора?",
-        responses: [
-          {text: 'До 5,5 м3 в минуту'},
-          {text: 'от 5,5 – 13 м3 в минуту'},
-          {text: '13- 65 м3 в минуту'},
-          {text: 'Не знаю'},
-        ]
-      }
-    ]
-  };
-
-
-  new Vue({
-    el: '#apps',
-    data: {
-      quizs: quizs,
-      questionIndex: 0,
-      userResponses: Array(quizs.questions.length).fill(false),
-      answers: [],
-      completed: false
-    },
-    computed: {
-      percent: function () {
-        return ((this.questionIndex / this.quizs.questions.length) * 100) + '%'
-      },
-      percentStyle: function () {
-        return ((this.questionIndex / this.quizs.questions.length) * 100 + 5) + '%'
-      }
-    },
-    methods: {
-      next: function (e) {
-        var question = quizs.questions[this.questionIndex].text;
-        var answer = e.target.labels[0].innerText;
-        this.answers.push([question, answer]);
-        this.questionIndex++;
-      },
-      submit: function () {
-        this.completed = true;
-        $('[name=answers]').val(JSON.stringify(this.answers));
-      },
-      restart: function () {
-        this.answers = [];
-        this.questionIndex = 0;
-      }
-    }
-  });
-</script>
 
 <?/*
 		<div class="sec sec_equipment" id="equipment">
@@ -501,33 +245,7 @@ global $inc, $current, $refinfo, $cms_object, $root;
 
 
 
-<div class="sec sec_hook section" id="hook">
-    <div class="art container">
-        <div class="wrap">
-            <div class="head2">Есть вопросы, <br>мы вам перезвоним!</div>
-            <form action="<?= $inc->send; ?>" method="post">
-                <input type="hidden" name="goalname" value="Есть вопросы">
-                <input type="hidden" name="goal" value="goal_questions">
-                <input type="hidden" name="good" value="">
-                <div class="items columns">
-                    <div class="item column"><input type="text" name="name" placeholder="Ваше имя"></div>
-                    <div class="item column"><input type="text" name="phone" placeholder="+7 (____) - ___ - ___ - _">
-                    </div>
-                </div>
-                <div class="columns items">
-                    <div class="item column"><input type="text" name="email" placeholder="E-mail"></div>
-                    <div class="item column">
-                        <button type="submit"><span>Отправить письмо</span></button>
-                    </div>
-                </div>
-                <label>
-                    <input type="checkbox" name="politic" value="1" data-focus="icon" checked>
-                    <span class="icon"></span><span class="label">Я даю согласие на обработку персональных данных</span>
-                </label>
-            </form>
-        </div>
-    </div>
-</div>
+
 
 <?
 $buyouts = array (
@@ -699,6 +417,34 @@ $buyouts = array (
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+
+<div class="sec sec_hook section" id="hook">
+    <div class="art container">
+        <div class="wrap">
+            <div class="head2">Есть вопросы, <br>мы вам перезвоним!</div>
+            <form action="<?= $inc->send; ?>" method="post">
+                <input type="hidden" name="goalname" value="Есть вопросы">
+                <input type="hidden" name="goal" value="goal_questions">
+                <input type="hidden" name="good" value="">
+                <div class="items columns">
+                    <div class="item column"><input type="text" name="name" placeholder="Ваше имя"></div>
+                    <div class="item column"><input type="text" name="phone" placeholder="+7 (____) - ___ - ___ - _">
+                    </div>
+                </div>
+                <div class="columns items">
+                    <div class="item column"><input type="text" name="email" placeholder="E-mail"></div>
+                    <div class="item column">
+                        <button type="submit"><span>Отправить письмо</span></button>
+                    </div>
+                </div>
+                <label>
+                    <input type="checkbox" name="politic" value="1" data-focus="icon" checked>
+                    <span class="icon"></span><span class="label">Я даю согласие на обработку персональных данных</span>
+                </label>
+            </form>
         </div>
     </div>
 </div>
